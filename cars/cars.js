11bb9194acab1dbbb19b0455c04aeaf643924ac5
Patricia <waltraute@hotmail.com>
2022-07-23 19:10:42 +0200

    constructor

---
+++ cars/cars.js
@@ -0,0 +1,29 @@
+/* Necesitamos registrar datos de coches:
+
+- Cilindrada
+- Marca
+- Matrícula
+- Tipo combustible (gasolina, gasoil, híbrido, eléctrico)
+- Color
+
+Crea un array con 6 coches (con los parámetros que quieras). 
+
+Setea el color de los coches en posiciones pares a "azul".
+
+Setea el color de los coches en posiciones impares a "rojo".
+
+Muestra la marca de los coches que tienen combustible de tipo "gasolina".  */
+
+class Cars {
+
+constructor(cilindrada, marca, matricula, tipoCombustible, color) {
+    this.cilindrada = cilindrada;
+    this.marca = marca;
+    this.matricula = matricula;
+    this.tipoCombustible = tipoCombustible;
+    this.color = color;
+}
+
+
+
+}
