15f22566991de7251d987b4a495731f482815bb9
Patricia <waltraute@hotmail.com>
2022-07-23 19:10:29 +0200

    updated

--- promises/date.js
+++ promises/date.js
@@ -1,3 +1,22 @@
+/*
+Escriu una funció que retorni promeses que es resolen en un temps aleatori entre 0 i 5 segons. Executa-la 10 vegades de 
+manera que puguis indentificar quin és l'ordre en el que s'han executat i el temps en el que s'han resolt, escrivint a un 
+array les dues coses cada vegada que es resolgui una, de manera que a l'array contingui algo així:
+
+{ num: 8, time: '515' } 
+{ num: 7, time: '626' } 
+{ num: 6, time: '867' } 
+{ num: 9, time: '1047' } 
+{ num: 1, time: '1402' } 
+{ num: 0, time: '2864' } 
+{ num: 2, time: '3153' } 
+{ num: 5, time: '3789' } 
+{ num: 4, time: '3948' } 
+{ num: 3, time: '4004' }
+
+*/
+
+
 let datos = [];
 
 const prova = (num) => {
