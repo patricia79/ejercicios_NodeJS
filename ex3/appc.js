3c7bd39b04c1a58511b42730e9367df116b52a24
Patricia <waltraute@hotmail.com>
2022-07-24 10:30:25 +0200

    updated

--- ex3/appc.js
+++ ex3/appc.js
@@ -17,39 +17,46 @@ Muestra la marca de los coches que tienen combustible de tipo "gasolina".  */
 let Coche = require("./c.js");
 
 try {
+  // inicialitza l'array de 6 cotxes
 
-     // inicialitza l'array de 6 cotxes
+  arrayCoches = [
+    new Coche(2000, "BMW", "2451GJD", "lila", "benzina"),
+    new Coche(2000, "Audi", "2452GJD", "taronja", "g-oil"),
+    new Coche(2000, "Lexus", "2453GJD", "blanc", "hibrid"),
+    new Coche(2000, "Mercedes-Benz", "2454GJD", "negre", "electric"),
+    new Coche(2000, "Tesla", "2455GJD", "verd", "benzina"),
+    new Coche(2000, "Porsche", "2456GJD", "groc", "g-oil"),
+  ];
 
-     arrayCoches = [
-          new Coche(2000, "BMW", "2451GJD", "rojo", "g"),
-          new Coche(2000, "Audi", "2452GJD", "rojo", "g-oil"),
-          new Coche(2000, "Lexus", "2453GJD", "rojo", "h"),
-          new Coche(2000, "Mercedes-Benz", "2454GJD", "rojo", "e"),
-          new Coche(2000, "Tesla", "2455GJD", "rojo", "g"),
-          new Coche(2000, "Porsche", "2456GJD", "rojo", "g-oil"),
-
-     ];
-
-     // un coche que fallarà
-     let cocheIncorrecto = new Coche(2000, "BMW", "2450GJD", "rojo", "ggoil");
-     console.log(cocheIncorrecto);
-
+  // un coche que fallarà
+  let cocheIncorrecto = new Coche(2000, "BMW", "2450GJD", "marro", "ggoil");
+  console.log(cocheIncorrecto);
 } catch (error) {
-     console.log("ERROR: " + error.message);
+  console.log("ERROR: " + error.message);
 }
 
 console.log(arrayCoches);
 
 for (const coche of arrayCoches) {
-    if (coche.combustible == "g") {
-         console.log(coche.marca);
-    }
+  if (coche.combustible == "benzina") {
+    console.log(`Els cotxes que van amb el tipus de combustible: ${coche.combustible}, són : ${coche.marca}`);
+  }
 }
+/*
+for (let index = 0; index < arrayCoches.length; index++) {
+  if (arrayCoches[index].combustible == "g") {
+    console.log(arrayCoches[index].combustible);
+  }
+} 
+*/
 
 for (let index = 0; index < arrayCoches.length; index++) {
-    if (arrayCoches[index].combustible == "g") {
-         console.log(arrayCoches[index].combustible);
-    }
+  if (index % 2 == 0) {
+    console.log((arrayCoches[index].color = "vermell"));
+  } else {
+    console.log((arrayCoches[index].color = "blau"));
+  }
+ 
 }
 /*
 haces un metodo setCoche(){
@@ -58,5 +65,7 @@ un for
 que recorra el array
 If ( i % 2 == 0 ) arrayCoches[i].color = "azul"
 If ( i % 2 == 1 ) arrayCoches[i].color = "rojo" 
-
+ if (index % 2 == 1) {
+    console.log((arrayCoches[index].color = "azul"));
+  }
 */
--- ex3/c.js
+++ ex3/c.js
@@ -8,7 +8,7 @@ class Coches {
     this.matrícula = matrícula
     this.color = color
     this.combustible = combustible
-    this.tipus_combustible =  ["g", "g-oil", "h", "e"];
+    this.tipus_combustible =  ["benzina", "g-oil", "hibrid", "electric"];
     
     if (!this.tipus_combustible.includes(combustible)){
         throw new Error(tipus_combustible + " no és un combustible")
